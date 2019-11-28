eff9111714bc0d81f40ecf024837c3095cecadcc
Leandro Gioria <legioria@hotmail.com>
2019-11-28 00:52:09 -0300

    stylized perfil page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-    return <h1>Profile</h1>;
+    const profile = useSelector(state => state.user.profile);
+
+    function handleSubmit(data) {}
+
+    return (
+        <Container>
+            <Form initialData={profile} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Nome Completo" />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Seu endereço de e-mail"
+                />
+
+                <hr />
+
+                <Input
+                    name="oldPassoword"
+                    type="password"
+                    placeholder="Sua senha atual"
+                />
+                <Input
+                    name="passoword"
+                    type="password"
+                    placeholder="Nova senha"
+                />
+                <Input
+                    name="confirmPassoword"
+                    type="password"
+                    placeholder="Confirmação de senha"
+                />
+
+                <button type="submit">Atualizar perfil</button>
+            </Form>
+
+            <button type="button">Sair do GoBarber</button>
+        </Container>
+    );
 }
