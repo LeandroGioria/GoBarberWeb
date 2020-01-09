f95ad2f090943226cb349a6761e7e03e4d26406e
Leandro Gioria <legioria@hotmail.com>
2020-01-09 18:32:22 -0300

    Fixed blank line

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-    height: 100vh;
+    height: 100%;
+    overflow-y: auto;
     background: linear-gradient(-150deg, #7159c1, #ab59c1);
 `;
